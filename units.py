e47c12a4f5faac164b33737fbc59a52f71d26ca6
dzamakhaiev <bogart047@gmail.com>
2019-03-21 17:28:39 +0200

    refactor code

--- units.py
+++ units.py
@@ -6,8 +6,8 @@ class UnitFiles(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.positive_dict = {'test_path_0': {'f_size': 123}, 'test_path_1': {'f_size': 123}}
-        cls.negative_dict = {'test_path_0': {'f_size': 123}, 'test_path_1': {'f_size': 321}}
+        cls.equal_dict = {'test_path_0': {'f_size': 123}, 'test_path_1': {'f_size': 123}}
+        cls.not_equal_dict = {'test_path_0': {'f_size': 123}, 'test_path_1': {'f_size': 321}}
 
     def setUp(self):
         self.files_instance = duplicates.Files()
@@ -15,11 +15,11 @@ class UnitFiles(unittest.TestCase):
     def test_find_equal_files(self):
 
         with self.subTest(msg='Check dict with equal files by size'):
-            result = self.files_instance.find_equal_files(files=self.positive_dict)
+            result = self.files_instance.find_equal_files(files=self.equal_dict)
             self.assertTrue(result)
 
         with self.subTest(msg='Check dict with not equal files by size'):
-            result = self.files_instance.find_equal_files(files=self.negative_dict)
+            result = self.files_instance.find_equal_files(files=self.not_equal_dict)
             self.assertFalse(result)
 
 
