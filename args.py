8ebcadfb1f8963fcff36e4514cd9475ba8523471
dzamakhaiev <bogart047@gmail.com>
2019-04-01 16:41:24 +0300

    add arg parser

---
+++ args.py
@@ -0,0 +1,12 @@
+import argparse
+import sys
+
+
+parser = argparse.ArgumentParser(description='TBD: some description.')
+parser.add_argument('-p', '--path', required=True,  help='Path to directory to scan')
+parser.add_argument('-a', '--alg', choices=['sha1', 'sha256', 'sha512', 'md5'], default='sha1', help='Hashing algorithm')
+parser.add_argument('-u', '--unit', choices=['mb', 'gb', 'tb'], default='gb', help='Unit of measuring size of files')
+parser.add_argument('-m', '--max', type=int,  default=sys.maxsize, help='Max files to check in directory')
+parser.add_argument('-o', '--output', default='results.txt', help='Output results to txt file')
+parser.add_argument('-l', '--log', default='log.txt', help='File for logging')
+
