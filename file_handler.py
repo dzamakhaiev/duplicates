d783101c28aea63513a594b130f2eb07dca3686c
dzamakhaiev <bogart047@gmail.com>
2019-04-08 20:02:49 +0300

    add copy file

--- file_handler.py
+++ file_handler.py
@@ -14,6 +14,45 @@ def create_file(filename, n_bytes=1000):
         print(e)
 
 
+def copy_file(filename, n=1):
+    """
+    Copy exists file with +1 int the name, starting from 1.
+    Does not suppose to make a number of copies more than 9.
+    :param str filename:
+    :param int n: number of copies
+    """
+    copies = []
+    new_name = None
+    last_index = -5
+    digit = int(filename[last_index]) + 1 if str.isdigit(filename[last_index]) else 1
+
+    for i in range(n):
+
+        # first time in loop and first copy
+        if not new_name and digit == 1:
+            new_name, ext = filename.split('.')
+            new_name += '_{}.{}'.format(digit, ext)
+            copies.append(new_name)
+
+        # first time in loop and not first copy
+        if not new_name and digit > 1:
+            new_name = filename.replace(str(digit-1), str(digit))
+            digit += 1
+
+        else:
+            new_name = new_name.replace(str(digit-1), str(digit))
+            copies.append(new_name)
+            digit += 1
+
+        try:
+            shutil.copyfile(src=filename, dst=new_name)
+
+        except (OSError, PermissionError) as e:
+            print(e)
+
+    return copies
+
+
 def create_dir(dirname):
     try:
         os.mkdir(dirname)
