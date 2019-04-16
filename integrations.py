95d7dd065bcec64248676e5d1c2c48519d808c75
dzamakhaiev <bogart047@gmail.com>
2019-04-16 20:50:09 +0300

    add integration test

---
+++ integrations.py
@@ -0,0 +1,64 @@
+import os
+import unittest
+import duplicates
+import file_handler
+
+from test_input import TEST_DIR
+from test_input import TEST_FILE
+from test_input import FIND_CHECK
+
+
+class Integration(unittest.TestCase):
+
+    @staticmethod
+    def create_file_structure(input_dict):
+        # Create test dir
+        file_handler.create_dir(TEST_DIR)
+        old_dir = os.getcwd()
+        test_dir = os.path.join(old_dir, TEST_DIR)
+        os.chdir(test_dir)
+
+        # Create file structure in current directory for test find method
+        file_handler.create_file_structure(file_structure=input_dict)
+        return old_dir, test_dir
+
+    @staticmethod
+    def delete_file_structure(old_dir, test_dir):
+        # Clean up
+        os.chdir(old_dir)
+        file_handler.delete_dir_recursively(test_dir)
+
+
+class IntegrationFiles(Integration):
+    """
+    Check intercommunication of methods from Files class
+    """
+
+    def setUp(self):
+        self.files_instance = duplicates.Files()
+
+    def test_files_class(self):
+        """
+        Method 'find' should return a dict with files and sizes.
+        Then method 'find_equal_files' should find equal by size files and return them in list.
+        """
+
+        for desc, input_dict, expected in FIND_CHECK:
+            with self.subTest(msg=desc):
+
+                # Create file structure in current directory for test find method
+                old_dir, test_dir = self.create_file_structure(input_dict=input_dict)
+                files = self.files_instance.find(top=test_dir)
+                equal_files = self.files_instance.find_equal_files(files)
+
+                # Clean up
+                self.delete_file_structure(old_dir, test_dir)
+
+                # Calculate expected list with equal files by size
+                sizes = [f_meta['f_size'] for f_meta in files.values()]
+                exp_list = [f_path for f_path, f_meta in files.items() if sizes.count(f_meta['f_size']) > 1]
+                self.assertEqual(equal_files, exp_list)
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
